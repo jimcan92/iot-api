266f19b1db2d4a6b12422dfa2ad790a36f830755
jimcan92 <jimcan051592@gmail.com>
2022-06-28 09:16:05 +0800

    ngano man ni oyy

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ dotenv.config({ path: './config/config.env' })
 
 connectDb();
 
-const PORT = process.env.PORT || 8080
-
 const app = express();
 
 app.use(cors())
-app.use(express.json({ extended: false }))
-app.use('/api/v1/iotdata', iotdata)
+app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/api/v1/iotdata', iotdata)
+
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.log(`Server running in port ${PORT}`))
