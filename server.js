f345def26322d1e4dd07615404bd9eeb168804bd
jimcan92 <jimcan051592@gmail.com>
2022-06-28 09:23:04 +0800

    no db and routes

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const connectDb = require('./config/db')
 
 dotenv.config({ path: './config/config.env' })
 
-connectDb();
+// connectDb();
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/api/v1/iotdata', iotdata)
+// app.use('/api/v1/iotdata', iotdata)
 
 const PORT = process.env.PORT || 8080
 
