eb8e2a6efab5962069458d628dfb9464f8a4eed4
jimcan92 <jimcan051592@gmail.com>
2022-06-28 09:27:02 +0800

    no public

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 // app.use('/api/v1/iotdata', iotdata)
 
 const PORT = process.env.PORT || 8080
